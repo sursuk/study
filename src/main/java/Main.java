99a85f55013029f144dba7c3321e487237d00c3e
Алексей Шемет <shemet162534@gmail.com>
2022-11-18 21:14:30 +0300

    1

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+//1110
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int lengthArray = scanner.nextInt();
+
+        int currentNum = scanner.nextInt();
+        System.out.println(currentNum);
+
+        for (int i = 0; i < lengthArray; i++) {
+            int temp = scanner.nextInt();
+            if(temp != currentNum) {
+                currentNum = temp;
+                System.out.println(currentNum);
+            }
+        }
+    }
+}
