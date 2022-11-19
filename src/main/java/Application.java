858cbad49f9056fce02e8df57c0d111d8e8418b6
Алексей Шемет <shemet162534@gmail.com>
2022-11-19 04:56:56 +0300

    1

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
+
+//1110
 public class Application {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int quantityElement = scanner.nextInt();
+
+        int counter = 0;
+        int resultCounter = 0;
+
+        for (int i = 0; i <= quantityElement; i++) {
+            if (scanner.nextLine().equals("1")) {
+                counter++;
+            } else {
+                if (counter > resultCounter) {
+                    resultCounter = counter;
+                }
+                counter = 0;
+            }
+        }
+        if (counter > resultCounter) {
+            resultCounter = counter;
+        }
+        System.out.println(resultCounter);
+    }
 }
